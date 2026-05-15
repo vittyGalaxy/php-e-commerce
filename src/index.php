68862b2af8f1d35dc46e8db4057843f9bde85eb1
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-05-15 23:17:45 +0200

    implemented index

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <title> Carrel Home</title>
+    </head>
+
+    <body>
+        <a href="pages/productInsertion.php"> Product Insertion </a>    
+        <br>
+    </body>
+</html>
