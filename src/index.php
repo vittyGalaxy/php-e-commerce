b55595a762d3cd595234b480cc3b87571d683269
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-05-18 00:00:52 +0200

    implemented index

--- src/index.php
+++ src/index.php
@@ -7,5 +7,9 @@
     <body>
         <a href="pages/productInsertion.php"> Product Insertion </a>    
         <br>
+        <a href="pages/viewProduct.php"> View Product </a>    
+        <br>
+        <a href="pages/searchProduct.php"> Search Product </a>    
+        <br>
     </body>
 </html>
