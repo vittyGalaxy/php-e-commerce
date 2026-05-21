76a3a5c13c4dc821e09438938ba5e1ec227654a6
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-05-21 23:51:24 +0200

    remove insertion

--- src/index.php
+++ src/index.php
@@ -5,8 +5,6 @@
     </head>
 
     <body>
-        <a href="pages/productInsertion.php"> Product Insertion </a>    
-        <br>
         <a href="pages/viewProduct.php"> View Product </a>    
         <br>
         <a href="pages/searchProduct.php"> Search Product </a>    
