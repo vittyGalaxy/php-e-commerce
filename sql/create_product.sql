cd2167c274dece6b31e1afc880faf2301c51d7ca
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-05-24 19:53:11 +0200

    create create_table

--- sql/create_product.sql
+++ sql/create_product.sql
@@ -1,5 +1,20 @@
 CREATE TABLE Product(
-    idProduct INT AUTO_INCREMENT PRIMARY KEY,
+    idProduct INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     name VARCHAR(100) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT NOT NULL
+)
+
+CREATE TABLE  User(
+    taxIdCode VARCHAR(100) PRIMARY KEY NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+)
+
+CREATE TABLE User_Cart(
+    idProduct       INT,
+    taxIdCode       VARCHAR(100),
+    FOREIGN KEY (idProduct)     REFERENCES Product(idProduct),
+    FOREIGN KEY (taxIdCode)     REFERENCES User(taxIdCode)
 )
